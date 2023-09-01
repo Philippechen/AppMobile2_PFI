b29a5635d8386be3eaea04231f16cc1772135fb5
Allan Joshua <allanjoshuaf@gmail.com>
2023-09-01 22:10:35 +0300

    ajout mdp, modal.js et fonction modal dans login

--- components/login.js
+++ components/login.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Modal } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -12,6 +12,7 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 */
 import EntrepotsScreen from './entrepots'
 import {PanierScreen, ProduitsScreen,PanierContextProvider} from './panier'
+import {SimpleModal} from './modal';
 
 const ManageScreen = () => {}
 
@@ -42,39 +43,85 @@ const ContactUsScreen = () => (
 
 // page homeScreen (pt creer fichier pour lui ?)
 const HomeScreen = ({ navigation }) => {
-  // pour la db le useState. 
-  const [users, setUsers] = useState([]);
+   
+  const [users, setUsers] = useState([]); // pour la db le useState.
+  const [isModalVisible, setIsModalVisible] = useState(false);    //pour Modal
+  const [chooseData, setChooseData] = useState();                 //pour Modal
+  const [selectedUser, setSelectedUser] = useState(null);         
   const db = new Database("users");
+
+  const changeModalVisible = (bool) => {
+    setIsModalVisible(bool);
+  }; //pour Modal
+
+  const setData = (data) => {
+    setChooseData(data);
+  }
 
   // onPress button -> envoie dans page produit electroniques (pt creer fichier pour lui ?)
   const handleValidationElec = () => {
     navigation.navigate('Produits: Électroniques');
   };
 
- // Récupérer les données des utilisateurs depuis la base de données pour les afficher à l'écran
+  const handleUserSelected = (user) => {
+    changeModalVisible(true);
+    setSelectedUser(user); // Mettre à jour l'état avec l'utilisateur sélectionné
+  }
+
+  // Récupérer les données des utilisateurs depuis la base de données pour les afficher à l'écran
   useEffect(() => {
-    db.execute("CREATE TABLE IF NOT EXISTS users (id_user TEXT PRIMARY KEY, nom TEXT, admin BOOLEAN);")
-      .then(() => db.execute("SELECT * FROM users"))
-      .then(resultSet => {
-        console.log("Result set:", resultSet);  
-        setUsers(resultSet.rows);
-      })
-      .catch(error => {
-        console.error("An error occurred:", error);
-      });
+  //db.execute("DROP TABLE IF EXISTS users")  // Supprime la table si elle existe
+  //.then(() => db.execute("CREATE TABLE IF NOT EXISTS users (id_user TEXT PRIMARY KEY, nom TEXT, admin BOOLEAN, mdp TEXT)"))
+  db.execute("CREATE TABLE IF NOT EXISTS users (id_user TEXT PRIMARY KEY, nom TEXT, admin BOOLEAN, mdp TEXT);")
+    //.then(() => db.execute("DELETE FROM users")) // Optionnel : supprime les données existantes
+    // .then(() => db.execute("INSERT INTO users (id_user, nom, admin, mdp) VALUES ('00', 'Allan Fournier', 1, 'Allan')"))
+    // .then(() => db.execute("INSERT INTO users (id_user, nom, admin, mdp) VALUES ('01', 'Enlong Chen', 1, 'Enlong')"))
+    // .then(() => db.execute("INSERT INTO users (id_user, nom, admin, mdp) VALUES ('02', 'Lina Jabour', 0, 'Lina')"))
+    // .then(() => db.execute("INSERT INTO users (id_user, nom, admin, mdp) VALUES ('03', 'Ava Newman', 0, 'Ava')"))
+    // .then(() => db.execute("INSERT INTO users (id_user, nom, admin, mdp) VALUES ('04', 'Liev Newman', 0, 'Liev')"))
+    // .then(() => db.execute("INSERT INTO users (id_user, nom, admin, mdp) VALUES ('05', 'Elizaveta Titova', 0, 'Elizaveta')"))
+    // .then(() => db.execute("INSERT INTO users (id_user, nom, admin, mdp) VALUES ('06', 'Ashkel Zemiya', 0, 'Ashkel')"))
+    .then(() => db.execute("SELECT * FROM users"))
+    .then(resultSet => {
+      console.log("Result set:", resultSet);
+      setUsers(resultSet.rows);
+    })
+    .catch(error => {
+      console.error("An error occurred:", error);
+    });
   }, []);
 
   return (
     <View style={styles.container}>
+      <Text style={styles.text}>
+        {chooseData}
+      </Text>
       {users.map(user => (
         <Pressable 
-        key={user.id_user} 
-        style={styles.pressable}
-        onPress={handleValidationElec}
-        >
-          <Text style={styles.pressableText}>{user.nom}</Text>
+          key={user.id_user} 
+          style={styles.pressable}
+          onPress={() =>  {
+            changeModalVisible(true)
+            handleUserSelected(user);
+          }}>
+          <Text style={styles.pressableText}>
+            {user.nom}
+          </Text>
         </Pressable>
       ))}
+      <Modal
+        transparent={true}
+        animationType="fade"
+        visible={isModalVisible}
+        nRequestClose={()=>changeModalVisible(false)}
+      >
+        <SimpleModal 
+          changeModalVisible={changeModalVisible}
+          setData={setData}
+          selectedUser={selectedUser}
+          handleValidationElec={handleValidationElec}
+        />
+      </Modal>
     </View>
   );
 };
@@ -219,4 +266,9 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize: 18,
       },
+      text: {
+        marginVertical: 20,
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
   });
---
+++ components/modal.js
@@ -0,0 +1,120 @@
+//Modal pour le login 
+//01/09/23
+
+import {useState} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity, Dimensions, TextInput} from 'react-native';
+
+//dimension 
+const WIDTH = Dimensions.get('window').width;
+const HEIGHT_MODAL = 180;
+
+const SimpleModal = (props) => {
+    const selectedUser = props.selectedUser;
+    const [password, setPassword] = useState('');
+    const [passwordFailed, setPasswordFailed] = useState(false);
+
+    closeModal = (bool, data) => {
+        
+        if (password === selectedUser.mdp) {
+            setPasswordFailed(false);
+            props.changeModalVisible(bool);
+            props.setData('Continuer');
+            props.handleValidationElec();
+        } else {
+            setPasswordFailed(true);
+            props.changeModalVisible(bool);
+            props.setData('Mot de passe incorrect');
+        }
+    }
+    
+    return(
+        <TouchableOpacity
+            disabled={true}
+            style={styles.container}
+        >
+            <View style={styles.modal}>
+                <View style={styles.textView}>
+                    <Text 
+                        style={[styles.text, {fontSize:20} ]}>
+                        Bienvenue
+                    </Text>
+                    <Text 
+                        style={styles.text}>
+                        Entrer votre mot de passe
+                    </Text>
+                    <TextInput 
+                        style={styles.textInput}
+                        placeholder="••••••••"
+                        secureTextEntry={true}
+                        onChangeText={text => setPassword(text)}
+                        value={password}
+                    />
+                </View>
+                <View style={styles.buttonsView}>
+                    <TouchableOpacity 
+                        style={styles.touchableOpacity}
+                        onPress={() => closeModal(false, 'Vous avez annulé')}    
+                    >
+                        <Text 
+                            style={[styles.text, {color: 'blue'}]}>
+                            Annuler
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity 
+                        style={styles.touchableOpacity}
+                        onPress={() => closeModal(false, 'Continuer')} 
+                    >
+                        <Text 
+                            style={[styles.text, {color: 'blue'}]}>
+                            Continuer
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        alignItems:'center',
+        justifyContent: 'center'
+    },
+    modal: {
+        height: HEIGHT_MODAL,
+        width: WIDTH - 80,
+        paddingTop: 10,
+        backgroundColor: 'white',
+        borderRadius: 10,
+    },
+    textView: {
+        flex:1,
+        alignItems:'center',
+    },
+    text: {
+        margin: 5,
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    buttonsView: {
+        width: '100%',
+        flexDirection: 'row',
+    },
+    touchableOpacity:{
+        flex: 1,
+        paddingVertical: 10,
+        alignItems: 'center',
+    },
+    textInput: {
+        width: '80%',
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginTop: 10,
+        paddingHorizontal: 10,
+        borderRadius: 5,
+    },
+})
+
+export {SimpleModal};
